292d5d1b3a541c275c27efd7b6ecd0889c73d091
x-vin <64435973+x-vin@users.noreply.github.com>
2022-08-19 15:33:29 +0100

    S08 - full width key details bar (#2)
    * Singe page view with accordions
    
    * Full width key details bar

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Appeal response',
+  serviceName: 'Prepare an appeal response',
 
   // Default port that prototype runs on
   port: '3000',
